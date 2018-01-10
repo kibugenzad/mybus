45e1e6e040adbe95e1fb767c9e38bafe8ce4a3cb
Kibugenza Didier <kibugenzad@gmail.com>
2018-01-10 14:51:20 +0200

    no message

--- api/mapData.js
+++ api/mapData.js
@@ -6,51 +6,47 @@ const moment = require('moment');
 const db = require("../db/connection")
 
 router.get("/", function (req, res) {
-    console.log(req.query)
 
-    if (req.query.latitude !== undefined || req.query.latitude !== undefined){
-        if (req.query.plate_number !== ''){
+})
+
+router.post("/", function (req, res) {
+    if (req.body.latitude !== undefined || req.body.latitude !== undefined) {
+        if (req.body.plate_number !== '') {
             const sql_check_bus = "SELECT bus_key FROM bus_map_data WHERE plate_number = ?";
-            db.connection.query(sql_check_bus, [req.query.plate_number], function (err, result) {
-                if (err){
-                    return console.log("select",err.stack)
+            db.connection.query(sql_check_bus, [req.body.plate_number], function (err, result) {
+                if (err) {
+                    return console.log("select", err.stack)
                 }
                 console.log(result)
-                if (result.length > 0){
+                if (result.length > 0) {
                     //update bus record
                     const sql_update = "UPDATE bus_map_data SET latitude = ?, longitude = ?, title = ?, description = ? WHERE bus_key = ?";
-                    db.connection.query(sql_update, [req.query.latitude,req.query.latitude, result.title, result.description, result[0].bus_key], function (err, updated_res) {
-                        if (err){
-                            return console.log("update",err.stack)
+                    db.connection.query(sql_update, [req.body.latitude, req.body.latitude, result.title, result.description, result[0].bus_key], function (err, updated_res) {
+                        if (err) {
+                            return console.log("update", err.stack)
                         }
 
                         console.log("updated")
                     })
-                }else{
-                    if (req.query.latitude !== '' && req.query.longitude !== ''){
+                } else {
+                    if (req.body.latitude !== '' && req.body.longitude !== '') {
                         let mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
                         const bus_key = randtoken.generate(250);
                         const sql = "INSERT INTO `bus_map_data` (`id`,`bus_key`,`plate_number`,`latitude`,`longitude`,`title`,`description`,`last_update`,`date_created`) VALUES (0, ?, ? ,?, ?, ?, ?, ?, ?)";
-                        db.connection.query(sql, [bus_key,req.query.plate_number,req.query.latitude,req.query.longitude,req.query.title,req.query.description, mysqlTimestamp,mysqlTimestamp], function (err, result) {
-                            if (err){
+                        db.connection.query(sql, [bus_key, req.body.plate_number, req.body.latitude, req.body.longitude, req.body.plate_number, req.body.description, mysqlTimestamp, mysqlTimestamp], function (err, result) {
+                            if (err) {
                                 return console.log(err.stack)
                             }
 
                             console.log("inserted")
                         })
-                    }else{
+                    } else {
                         console.log("coordinates is empty")
                     }
                 }
             })
         }
-    }else{
-        console.log("no params")
     }
-})
-
-router.post("/", function (req, res) {
-
 })
 
 module.exports = router;
