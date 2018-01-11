0971132d585f5317abe8a694e287ab1c5bf928fd
Kibugenza Didier <kibugenzad@gmail.com>
2018-01-11 22:29:21 +0200

    no message

--- config/firebase.js
+++ config/firebase.js
@@ -6,4 +6,4 @@ const firebase = admin.initializeApp({
     databaseURL: "https://mybus-191611.firebaseio.com"
 });
 
-module.exports.firebase = firebase;
+module.exports.firebase = firebase.database();
