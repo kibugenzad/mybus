92ddb8402cf315ddef64361b797bf9dc61cefc56
Kibugenza Didier <kibugenzad@gmail.com>
2018-01-11 22:35:02 +0200

    no message

--- config/firebase.js
+++ config/firebase.js
@@ -4,6 +4,6 @@ var serviceAccount = require("./serviceAccountKey.json");
 const firebase = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://mybus-191611.firebaseio.com"
-});
+},"MyBus");
 
 module.exports.firebase = firebase;
